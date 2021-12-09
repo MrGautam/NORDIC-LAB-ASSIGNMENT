3a9b6f58aad7b93b94888499ec72315da34ed779
Akash Gautam <68077850+MrGautam@users.noreply.github.com>
2021-12-09 04:27:19 -0800

    Add files via upload

---
+++ Lab 5/L5Q5class.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L5Q5Class
+{
+	public class Product
+	{
+
+		public int ProductCode { get; set; }
+		public string ProductName { get; set; }
+		public string Category { get; set; }
+
+	}
+}
