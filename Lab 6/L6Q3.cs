54e95072705217c4227f0b902b94d995e340cf53
Akash Gautam <68077850+MrGautam@users.noreply.github.com>
2021-12-14 03:57:28 -0800

    Add files via upload

---
+++ Lab 6/L6Q3.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L6Q2
+{
+    class SimpleIntrest
+    {
+        public void SI(int year, double principal, double rate )
+        {
+
+            double si;
+            si = principal*rate*year/100;
+            Console.WriteLine( si );
+        }
+        static void Main(string[] args)
+        {
+
+            SimpleIntrest simpleIntrest = new SimpleIntrest();
+            Console.Write("Enter The Principal Amount : ");
+            double p = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter The Number of Years : ");
+            int t = Convert.ToInt16(Console.ReadLine());
+            Console.Write("Enter the Intrest Rate Of Interest : ");
+            double r = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Simple Intrest is :");
+            simpleIntrest.SI(t, p, r);
+            Console.ReadLine();
+        }
+    }
+}
